66698648c47ef4b97d44a6398f957510f50a91be
Feliu <felix1991palm@gmail.com>
2025-05-13 14:53:35 +0200

    Updated formatting

--- Point.hs
+++ Point.hs
@@ -1,6 +1,7 @@
 module Point (Point (..), getX, getY) where
 
 type X = Double
+
 type Y = Double
 
 data Point = Point X Y deriving (Show, Eq, Ord)
--- Types.hs
+++ Types.hs
@@ -1,10 +1,11 @@
-module Types (Quadrant(..), AngleType(..), Radians, Degrees) where
+module Types (Quadrant (..), AngleType (..), Radians, Degrees) where
 
-    type Radians = Double
-    type Degrees = Double
+type Radians = Double
 
-    data Quadrant = Q1 | Q2 | Q3 | Q4 | Origin | OnXAxis | OnYAxis 
-        deriving (Show)
+type Degrees = Double
 
-    data AngleType = InDegrees | InRadians 
-        deriving (Eq, Show)
+data Quadrant = Q1 | Q2 | Q3 | Q4 | Origin | OnXAxis | OnYAxis
+  deriving (Show)
+
+data AngleType = InDegrees | InRadians
+  deriving (Eq, Show)
