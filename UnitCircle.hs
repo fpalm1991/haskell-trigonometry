fa86b9d8a7066ca1111bb75aad8925eed4cfd419
Feliu <felix1991palm@gmail.com>
2025-05-15 18:12:52 +0200

    Fixed return type

--- UnitCircle.hs
+++ UnitCircle.hs
@@ -4,6 +4,6 @@ import Angle
 import AngleFunctions
 import Types
 
-getCoordinatesOnUnitCircle :: Angle -> (Radians, Radians)
+getCoordinatesOnUnitCircle :: Angle -> (Double, Double)
 getCoordinatesOnUnitCircle (Angle value InRadians) = (cos value, sin value)
 getCoordinatesOnUnitCircle (Angle value InDegrees) = (cos $ toRadians value, sin $ toRadians value)
